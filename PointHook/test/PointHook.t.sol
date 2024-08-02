26486e3ec120ec2c3f9804debe59bed29a3e162d
RonexLemon <ronexondimu@gmail.com>
2024-08-02 17:27:08 +0300

    start hook test

---
+++ PointHook/test/PointHook.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {Deployers} from "@uniswap/v4-core/test/utils/Deployers.sol";
+import {PoolSwapTest} from "v4-core/test/PoolSwapTest.sol";
+import {MockERC20} from "solmate/src/test/utils/mocks/MockERC20.sol";
+
+import {PoolManager} from "v4-core/PoolManager.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+
+import {Currency, CurrencyLibrary} from "v4-core/types/Currency.sol";
+
+import {Hooks} from "v4-core/libraries/Hooks.sol";
+import {TickMath} from "v4-core/libraries/TickMath.sol";
+import {SqrtPriceMath} from "v4-core/libraries/SqrtPriceMath.sol";
+import {LiquidityAmounts} from "@uniswap/v4-core/test/utils/LiquidityAmounts.sol";
+
+import "forge-std/console.sol";
+import {PointsHook} from "../src/PointHook.sol";
+contract TestPointHook is Test,Deployers{
+
+}
+
+
+
